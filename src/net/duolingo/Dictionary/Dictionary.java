2ad3d7f8780b380dd2cb6fadc4d264a54b463849
Saúl Arteaga <alojera15@gmail.com>
2023-05-11 20:42:22 +0100

    feat(Dictionary.java): created method eraseWord

--- src/net/duolingo/Dictionary/Dictionary.java
+++ src/net/duolingo/Dictionary/Dictionary.java
@@ -11,7 +11,8 @@ public class Dictionary {
 
     private Map<String, Set<String>> Dictionary = new HashMap<>();
 
-    public Dictionary() {}
+    public Dictionary() {
+    }
 
     public Map<String, Set<String>> getDictionary() {
         return Dictionary;
@@ -21,23 +22,38 @@ public class Dictionary {
         Dictionary = dictionary;
     }
 
-    public void insertWord(){
+    public void insertWord() {
         System.out.println("Write a word to add it to the dictionary: ");
         String addedWord = keyboard.nextLine();
         String formatedWord = addedWord.substring(0, 1).toUpperCase() + addedWord.substring(1);
         String indexWord = addedWord.substring(0, 1).toLowerCase();
 
         if (Dictionary.containsKey(indexWord)) {
-            Set<String> tempSet = Dictionary.get(indexWord); 
+            Set<String> tempSet = Dictionary.get(indexWord);
             tempSet.add(formatedWord);
             Dictionary.put(indexWord, tempSet);
         } else {
-            Set<String> tempSet = new HashSet<>(); 
+            Set<String> tempSet = new HashSet<>();
             tempSet.add(formatedWord);
             Dictionary.put(indexWord, tempSet);
         }
-        System.out.println("The word " + addedWord + " has been added to the dictionary.");
+        System.out.println("The word " + formatedWord + " has been added to the dictionary.");
     }
 
+    public void eraseWord() {
+        System.out.println("Write a word to erase it: ");
+        String erasedWord = keyboard.nextLine();
+        String formatedErasedWord = erasedWord.substring(0, 1).toUpperCase() + erasedWord.substring(1);
+        String indexWord = erasedWord.substring(0, 1).toLowerCase();
+
+        if (Dictionary.containsKey(indexWord)) {
+            Set<String> tempSet = Dictionary.get(indexWord);
+            tempSet.remove(formatedErasedWord);
+            Dictionary.put(indexWord, tempSet);
+        } else {
+            System.out.println("The word doesn't exist in this dictionary.");
+        }
+        System.out.println("The word " + formatedErasedWord + " has been erased from the dictionary.");
+    }
 
 }
